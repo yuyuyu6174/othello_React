dbaac54a05c78b5fc0ebb4c0c861f900ec2c949d
yuyuyu6174 <yuyuyu6174@gmail.com>
2025-06-24 19:28:24 +0900

    Add build time display

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/othello_React/',
   plugins: [react()],
+  define: {
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
+  },
 })
